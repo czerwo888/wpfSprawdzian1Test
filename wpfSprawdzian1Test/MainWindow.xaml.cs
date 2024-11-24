ae456cc8bef341cfa06bf2f6c885eb0074f535aa
czerwo888 <matben005@gmail.com>
2024-11-24 14:45:13 +0100

    init

---
+++ wpfSprawdzian1Test/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfSprawdzian1Test;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void StworzPrzycisk_OnClick(object sender, RoutedEventArgs e)
+    {
+        if(int.TryParse(textBox.Text, out int count) && count >= 1 && count <= 3)
+        {
+            nowePrzyciski.Children.Clear();
+            for (int i = 1; i <= count; i++)
+            {
+                Button button = new Button()
+                {
+                    Content = i.ToString(),
+                    Margin = new Thickness(5)
+                };
+                nowePrzyciski.Children.Add(button);
+            }
+        }
+        else
+        {
+            MessageBox.Show("Podaj wartość od 1 do 3", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void Slider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        stworzPrzycisk.FontSize = slider.Value;
+    }
+
+    private void WpiszButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        NoweOkno noweOkno = new NoweOkno();
+        if (noweOkno.ShowDialog() == true)
+            wyswietlTekst.Text = NoweOkno.wprowadzonyTekst;
+    }
+}
---
+++ wpfSprawdzian1Test/NoweOkno.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace wpfSprawdzian1Test;
+
+public partial class NoweOkno : Window
+{
+    public NoweOkno()
+    {
+        InitializeComponent();
+    }
+    
+    public static string wprowadzonyTekst { get; private set; }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        wprowadzonyTekst = textDrugieOkno.Text;
+        DialogResult = true;
+        Close();
+    }
+}
